2d742f36fcd063bdb36da756ad9755e75a9f56b5
gao mengnan <94237544@qq.com>
2024-07-31 17:38:10 +0800

    fff

--- lib/features/presentation/views/person/person_page.dart
+++ lib/features/presentation/views/person/person_page.dart
@@ -3,10 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:loanswift/core/common/widgets/widgets.dart';
 import 'package:loanswift/features/presentation/bloc/auth/auth_bloc.dart';
-import 'package:loanswift/features/presentation/views/person/logined_page.dart';
-import 'package:loanswift/theme/theme.dart';
 
-import '../../../../core/generated/l10n.dart';
+import '../../../../core/core.dart';
 
 class PersonPage extends StatefulWidget {
   const PersonPage({super.key});
@@ -19,54 +17,56 @@ class _PersonPageState extends State<PersonPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: AppText(
-          text: S.current.personal_info,
-          size: 17.sp,
-          // color: Pallete.whiteColor,
-        ),
-        centerTitle: true,
-        // backgroundColor: Pallete.backgroundColor,
-      ),
+      //appBar: AppBar(
+      //  title: AppText(
+      //    text: S.current.personal_info,
+      //    size: 17.sp,
+      //    // color: Pallete.whiteColor,
+      //  ),
+      //  centerTitle: true,
+      //  // backgroundColor: Pallete.backgroundColor,
+      //),
       body: BlocBuilder<AuthBloc, AuthState>(
         builder: (context, state) {
-          if (state.loginState.isLogined) {
-            return const LoginedPage();
-          } else {
-            return loginStateIdelWidget(context);
-          }
-        },
-      ),
-    );
-  }
-
-  // 已经登陆
-
-  Widget loginStateIdelWidget(BuildContext context) {
-    return Center(
-      child: OutlinedButton(
-        style: ElevatedButton.styleFrom(
-          elevation: 0,
-          side: BorderSide(
-            width: 1,
-            color: Colors.black.withOpacity(
-              0.1,
+          return SafeArea(
+            child: Container(
+              padding: const EdgeInsets.only(top: 30),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                //color: Colors.red,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.blue,
+                  child: Icon(
+                    size: 40,
+                    Icons.person_pin_circle_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+                //tileColor: Colors.orange,
+                onTap: () {
+                  Navigator.of(context).pushNamed("/auth");
+                },
+                title: Row(
+                  children: [
+                    AppText(
+                      text: S.current.login_register,
+                      size: 18.sp,
+                      textAlign: TextAlign.start,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    const Icon(
+                      Icons.arrow_right,
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
-          backgroundColor: Pallete.whiteColor,
-        ).copyWith(
-          foregroundColor: WidgetStateProperty.all<Color>(
-            Pallete.primaryColor,
-          ), // 设置字体颜色
-        ),
-        onPressed: () {
-          Navigator.pushNamed(context, '/auth');
+          );
         },
-        child: AppText(
-          text: S.current.login_register,
-          color: Pallete.primaryColor,
-          size: 16.sp,
-        ),
       ),
     );
   }
