f470797e38a5c11a8d5ca3951e501954e01187ab
gao <gao>
2024-08-01 06:24:02 +0800

    add server file

---
+++ server/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.json
+    return jsonify(data)
+
+@app.route('/middle/user/code', methods=['POST'])
+def sendcode():
+    print(request.content_type)
+    print(request.content_encoding)
+    print(request.get_data())
+
+    return "hello"
+    # data = request.json
+    # return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8008)
