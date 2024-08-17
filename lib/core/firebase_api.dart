70009d14802e1bf603e1c6f8f5859a1bec18aaa0
gao mengnan <94237544@qq.com>
2024-08-17 19:48:50 +0800

    * firebaseapi add

---
+++ lib/core/firebase_api.dart
@@ -0,0 +1,33 @@
+//import 'package:firebase_messaging/firebase_messaging.dart';
+//import 'package:flutter/material.dart';
+//
+//class FirebaseApi {
+//
+//  // create Instance
+//
+//  final _firebaseMessage = FirebaseMessaging.instance;
+//
+//
+//
+//  // to init notifications
+//
+//  Future<void> initNotifications() async {
+//
+//    // request permission from user
+//    await _firebaseMessage.requestPermission();
+//
+//    // fetch the FCM TOKEN
+//
+//    final fcmToken = await _firebaseMessage.getToken();
+//
+//    // print token
+//
+//    debugPrint(fcmToken);
+//
+//  }
+//
+//  // received messages
+//
+//  // init foreground and background
+//
+//}
